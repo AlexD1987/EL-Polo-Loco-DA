385ab7ad96b7559a170969c6cf288499651cb86d
Alexander Dause <alexander.dause@web.dem>
2023-10-09 20:24:26 +0200

    update level

--- models/world.class.js
+++ models/world.class.js
@@ -1,23 +1,8 @@
 class World {
     character = new Character();
-    enemy = [
-        new Enemy(),
-        new Enemy(),
-        new Enemy(),
-    ];
-    clouds = [
-        new Cloud()
-    ];
-    backgroundObject = [
-        new Ground('img/5_background/layers/air.png', 0),
-        new Ground('img/5_background/layers/3_third_layer/1.png', 0),
-        new Ground('img/5_background/layers/2_second_layer/1.png', 0),
-        new Ground('img/5_background/layers/1_first_layer/1.png', 0),
-        new Ground('img/5_background/layers/air.png', 719),
-        new Ground('img/5_background/layers/3_third_layer/2.png', 719),
-        new Ground('img/5_background/layers/2_second_layer/2.png', 719),
-        new Ground('img/5_background/layers/1_first_layer/2.png', 719)
-    ];
+    enemy = level1.enemies;
+    clouds = level1.clouds;
+    backgroundObject = level1.backgroundObjects;
 
     canvas;
     keyboard;
